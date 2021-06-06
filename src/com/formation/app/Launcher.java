34fada4eb2d983e4efd3151815ece1a69798f074
Dylan Salmon <dylan.salmon05@gmail.com>
2021-06-06 18:18:27 +0200

    06/06 18:21 ça avance bien

--- src/com/formation/app/Launcher.java
+++ src/com/formation/app/Launcher.java
@@ -2,9 +2,11 @@ package com.formation.app;
 
 //import com.formation.app.dao.DaoFactory;
 import com.formation.app.dao.jdbc.JdbcPlaceDao;
+import com.formation.app.dao.jdbc.JdbcTripDao;
 import com.formation.app.util.ConnectionManager;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Launcher {
 
@@ -12,116 +14,124 @@ public class Launcher {
         ConnectionManager.getConnection();
 
         JdbcPlaceDao placeQuery = new JdbcPlaceDao();
+        JdbcTripDao tripQuery = new JdbcTripDao();
 
-        List<Place> p = placeQuery.findAll();
-        for (int i = 0; i < p.size(); i++) {
-            System.out.println(p.get(i).getName());
-            System.out.println(p.get(i).getId());
-        }
+        List<Place> listPlaces = placeQuery.findAll();
+        List<Place> listTrips = placeQuery.findAll();
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean passageInTheLoop = true;
+
+        String input;
+
+        do {
+            System.out.println("What do you want to do ?");
+            System.out.println("1 - Add a place");
+            System.out.println("2 - Find a place");
+            System.out.println("3 - Edit a place");
+            System.out.println("4 - Remove a place");
+            System.out.println("5 - Add a trip");
+            System.out.println("6 - Find a trip");
+            System.out.println("7 - Remove a trip");
+            System.out.println("8 - Quit");
+            System.out.println();
+
+            input = scanner.nextLine();
+
+            switch (input) {
+                case "1":
+                    try {
+                        System.out.print("Name : ");
+                        input = scanner.nextLine();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                case "2":
+                    try {
+                        System.out.print("Id : ");
+                        input = scanner.nextLine();
+                        System.out.println(listPlaces.get(Integer.parseInt(input)).getName());
+                    } catch (Exception e) {
+                        System.err.println("Place not found");
+                        System.out.println();
+                    }
+                    break;
+
+                case "3":
+                    try {
+                        System.out.print("Id : ");
+                        input = scanner.nextLine();
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "4":
+                    try {
+                        System.out.print("Id : ");
+                        input = scanner.nextLine();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "5":
+                    try {
+
+                        System.out.print("Departure : please enter the id of the place : ");
+                        input = scanner.nextLine();
+
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                case "6":
+                    try {
+                        System.out.print("Id : ");
+                        input = scanner.nextLine();
+                        System.out.println(listTrips.get(Integer.parseInt(input)).getName());
+                    }
+                    catch (Exception e){
+                        System.err.println("Trip not found");
+                    }
+                    break;
+
+                case "7":
+                    try {
+                        System.out.print("Id : ");
+                        input = scanner.nextLine();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "8":
+                    System.out.println("Do you really want to quit ? (type 'yes' or 'y' to confirm)");
+                    input = scanner.nextLine();
+                    if(input.equals("yes") || input.equals("y"))
+                    {
+                        passageInTheLoop = false;
+                    }
+                    break;
+
+                default:
+                    System.out.println("The input is wrong : please retry");
+            }
+            System.out.println();
+
+        } while (passageInTheLoop);
+        System.out.println("Bye bye");
+
+
         ConnectionManager.closeConnection();
     }    //psvm
 
-
-
-//    Scanner scanner = new Scanner(System.in);
-//
-//    String input;
-//
-//        do {
-//        System.out.println("What do you want to do ?");
-//        System.out.println("1 - Add a place");
-//        System.out.println("2 - Find a place");
-//        System.out.println("3 - Edit a place");
-//        System.out.println("4 - Remove a place");
-//        System.out.println("5 - Add a trip");
-//        System.out.println("6 - Find a trip");
-//        System.out.println("7 - Remove a trip");
-//        System.out.println("8 - Quit");
-//        System.out.println();
-//
-//        input = scanner.nextLine();
-//
-//        switch (input) {
-//            case "1":
-//                try {
-//                    System.out.print("Name : ");
-//                    input = scanner.nextLine();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//
-//                break;
-//
-//            case "2":
-//                try {
-//                    System.out.print("Id : ");
-//                    input = scanner.nextLine();
-//                } catch (Exception e) {
-//                    System.err.println("Place not found");
-//                    System.out.println();
-//                }
-//                break;
-//
-//            case "3":
-//                try {
-//                    System.out.print("Id : ");
-//                    input = scanner.nextLine();
-//                }
-//                catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                break;
-//
-//            case "4":
-//                try {
-//                    System.out.print("Id : ");
-//                    input = scanner.nextLine();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                break;
-//
-//            case "5":
-//                try {
-//
-//                    System.out.print("Departure : please enter the id of the place : ");
-//                    input = scanner.nextLine();
-//
-//                }
-//                catch (Exception e){
-//                    e.printStackTrace();
-//                }
-//
-//                break;
-//
-//            case "6":
-//                try {
-//                    System.out.print("Id : ");
-//                    input = scanner.nextLine();
-//                }
-//                catch (Exception e){
-//                    System.err.println("Trip not found");
-//                }
-//                break;
-//
-//            case "7":
-//                try {
-//                    System.out.print("Id : ");
-//                    input = scanner.nextLine();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                break;
-//
-//            case "8":
-//                System.out.println("Do you really want to quit ? (retype 8 to confirm)");
-//                input = scanner.nextLine();
-//                break;
-//
-//            default:
-//                System.out.println("the input is wrong : please retry");
-//        }
-//        System.out.println();
-//
-//    } while (!input.equals("8"));
 }
